a74e93a9ebec3cc6d3424d71c848160e9959df53
kobts <kobts@yandex.ru>
2021-12-05 16:05:28 +0400

    Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String gender = "male";
+    String name = "Sergey";
+    int height = 177;
+    int weight = 61;
+    int age = 28;
+
+    void go() {
+
+    }
+
+    void run() {
+        
+    }
+
+    void sit() {
+        
+    }
+
+    void speak() {
+        
+    }
+
+    void learnJava() {
+        
+    }
+}
