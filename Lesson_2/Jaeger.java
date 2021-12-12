8fb41ce114701511a2c216e626677f80028cc4ed
kobts <kobts@yandex.ru>
2021-12-12 20:04:41 +0400

    Jaeger

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -7,6 +7,16 @@ public class Jaeger {
     private int strength;
     private int armor;
 
+    public Jaeger(String modelName, String mark, String origin, float height, float weight, int strength, int armor) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+        this.strength = strength;
+        this.armor = armor;
+    }
+
     public void setModelName(String modelName) {
         this.modelName = modelName;
     }
--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -1,21 +1,6 @@
 public class JaegerTest {
     public static void main(String[] args) {
-        Jaeger jaegerOne = new Jaeger();
-        jaegerOne.setModelName("Striker Eureka");
-        jaegerOne.setMark("Mark-5");
-        jaegerOne.setOrigin("Australia");
-        jaegerOne.setHeight(76.2f);
-        jaegerOne.setWeight(1.850f);
-        jaegerOne.setStrength(10);
-        jaegerOne.setArmor(9);
-
-        Jaeger jaegerTwo = new Jaeger();
-        jaegerTwo.setModelName("Gipsy Danger");
-        jaegerTwo.setMark("Mark-3");
-        jaegerTwo.setOrigin("United States of America");
-        jaegerTwo.setHeight(79.25f);
-        jaegerTwo.setWeight(1.980f);
-        jaegerTwo.setStrength(8);
-        jaegerTwo.setArmor(6);
+        Jaeger jaegerOne = new Jaeger("Striker Eureka", "Mark-5", "Australia", 76.2f, 1.850f, 10, 9);
+        Jaeger jaegerTwo = new Jaeger("Gipsy Danger", "Mark-3", "United States of America", 79.25f, 1.980f, 8, 6);
     }
 }
