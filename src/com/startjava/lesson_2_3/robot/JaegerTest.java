473a7e0b91cf08cb9943994645d324b311d11348
kobts <kobts@yandex.ru>
2021-12-31 17:46:41 +0400

    Packages

--- src/com/startjava/lesson_2_3/robot/JaegerTest.java
+++ src/com/startjava/lesson_2_3/robot/JaegerTest.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson_2_3.robot;
+
 public class JaegerTest {
     public static void main(String[] args) {
         Jaeger jaegerOne = new Jaeger("Striker Eureka", "Mark-5", "Australia", 76.2f, 1.850f, 10, 9);
